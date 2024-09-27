3de8e3d71c435805a956f14dbc7e1d4bfc144475
Ross Wilson <ross.wilson@otago.ac.nz>
2024-09-27 20:45:33 +0200

    add publications and conference presentations

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,13 +9,26 @@ const journalArticle = defineCollection({
       author: z.array(z.string()),
       volume: z.coerce.string(),
       issue: z.coerce.string().optional(),
-      pages: z.coerce.string(),
+      pages: z.coerce.string().optional(),
       year: z.coerce.string(),
       doi: z.string().optional()
     })
 });
 
+const conferencePresentation = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    conference: z.string(),
+    date: z.coerce.date(),
+    location: z.string(),
+    meetingdate: z.string(),
+    type: z.string().optional(),
+  })
+});
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   publications: journalArticle,
+  conferences: conferencePresentation,
 };
